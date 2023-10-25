bb199c63f7abfa008919b9136e17c9cfcf616785
Aswathi A K <aswathiak.3@gmail.com>
2023-10-25 13:03:59 +0530

    cors updated

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 
 //Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001','https://poshbo-admin.netlify.app'],
+  origin: ['http://localhost:3000', 'https://poshbo-admin.netlify.app','https://poshboonline.netlify.app/'],
   credentials: true,
 }));
 app.use(express.json({limit:"500mb"}));
--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -16,7 +16,7 @@ const verifyUserToken = (req, res, next) => {
 };
 
 const verifyAdminToken = (req, res, next) => {
-  const token = req.headers.authorization; console.log('token',token);
+  const token = req.headers.authorization; 
   if (!token) {
     return next(createError(401, "You are not authenticated"));
   }
